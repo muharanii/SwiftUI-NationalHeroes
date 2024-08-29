86bda66bac75f31dd563dc99e4708b3e808e8eb5
muharaniira <muharaniira@gmail.com>
2024-08-29 19:34:38 +0700

    Initial Commit

---
+++ SwiftUI-NationalHeroes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-NationalHeroes
+//
+//  Created by Ira Muharani on 29/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-NationalHeroes/SwiftUI_NationalHeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NationalHeroesApp.swift
+//  SwiftUI-NationalHeroes
+//
+//  Created by Ira Muharani on 29/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NationalHeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
